652e53ef95b3454a3491975c459efad7d21d9e52
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-03-23 19:10:36 +0300

    Update op_push.c

--- op_push.c
+++ op_push.c
@@ -1,38 +1,31 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * push - push element into the stack
- * @stack: stack given by main
- * @line_cnt: amount of lines
- *
- * Return: void
- */
-void push(stack_t **stack, unsigned int line_cnt)
+  * push - Adds a new node at the beginning of the stack
+  * @stack: The head of the stack
+  * @param: The value to adds on the stack
+  *
+  * Return: Nothing
+  */
+void push(stack_t **stack, unsigned int param)
 {
-	char *n = global.argument;
+	stack_t *new_node = NULL;
 
-	if ((is_digit(n)) == 0)
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+
+	new_node->n = param;
+	if (*stack)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-		exit(EXIT_FAILURE);
+		new_node->next = *stack;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+		return;
 	}
 
-	if (global.data_struct == 1)
-	{
-		if (!add_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		if (!queue_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
-	}
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
 }
